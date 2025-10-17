b1121a78e80645a9682d759633296c1929b0cf37
Mohammed-Abdelaziem <Mohammed.Abdullah05@eng-st.cu.edu.eg>
2025-10-17 14:56:35 +0300

    Initial commit

---
+++ src/pages/wallet/Wallet.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import WalletMiniNavBar from "../../components/WalletMiniNavBar";
+import WalletRecentActivity from "../../components/WalletRecentActivity";
+
+const Wallet = () => {
+  return (
+    <div className="flex flex-col items-center justify-center w-full min-h-screen bg-gray-50 pt-8">
+      {/* Wallet Info Section */}
+      <div className="p-4 w-1/2 text-center">
+        <h1 className="text-2xl font-bold mb-2 text-4xl">My Wallet</h1>
+        <p>
+          Manage your balance, view transactions, and track your loyalty rewards
+        </p>
+      </div>
+
+      {/* Mini Navbar Section */}
+      <div className="mt-6 w-[80%]">
+        <WalletMiniNavBar />
+      </div>
+    </div>
+  );
+};
+
+export default Wallet;
